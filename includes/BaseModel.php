74b8d2c9a659042bcce51f04db647f35cc418c26
Roger Dickey <dickeyr@gmail.com>
2012-12-21 22:36:12 -0600

    The BaseModel class will be the parent of any class that represents a database table, a model, for basic and advanced CRUD methods.

---
+++ includes/BaseModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Roger Dickey, Jr
+ * http://rogerdickeyjr.com/
+ * http://github.com/rogerd330/
+ * 12/21/12 10:30 PM
+ */
+
+class BaseModel {
+    private $dbh = null;
+
+    function create_db() {
+        $this->dbh = new PDO(sprintf('mysql:host=%s;dbname=%s', DB_HOST, DB_NAME), DB_USER, DB_PASS);
+    }
+
+    function destroy_db() {
+        $this->dbh = null;
+    }
+}
---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Roger Dickey, Jr
+ * http://rogerdickeyjr.com/
+ * http://github.com/rogerd330/
+ * 12/21/12 10:28 PM
+ */
+
+define('DB_NAME', 'database_name');
+define('DB_USER', 'database_user');
+define('DB_PASS', 'database_password');
+define('DB_HOST', 'database_hostname');
