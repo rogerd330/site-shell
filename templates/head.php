f98a01149b16e2c01d2e274d9fb38b37a4e2d398
Roger Dickey <dickeyr@gmail.com>
2013-02-13 12:33:28 -0600

    Trying to break my XHTML habbits. Removed unnecessary self-closing meta tags.

--- templates/head.php
+++ templates/head.php
@@ -18,21 +18,21 @@ list($page_name, $page_extension) = explode(".", basename($_SERVER["PHP_SELF"]))
 <!doctype html>
 <html>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset="utf-8">
         <title><?php echo sprintf("%s - %s", $page_title, $site_name) ?></title>
-        <link href="favicon.ico" type="image/x-icon" rel="icon" />
-        <link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="favicon.ico" type="image/x-icon" rel="icon">
+        <link href="favicon.ico" type="image/x-icon" rel="shortcut icon">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="<?php printf("%s", $meta_description) ?>">
         <meta name="keywords" content="<?php printf("%s", $meta_keywords) ?>">
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/text.css">
-        <link rel="stylesheet" href="css/960.css" />
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/960.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
 
