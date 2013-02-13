0a12660f34f500be918670882989ae2283fdbb87
Roger Dickey <dickeyr@gmail.com>
2013-02-13 12:56:12 -0600

    Trying to make the home page a little more informative for people who use this thing.

--- index.php
+++ index.php
@@ -13,20 +13,22 @@ include("templates/head.php");
 ?>
 
 <div class="grid_12">
-    <h1>Welcome!</h1>
+    <h1>Welcome to Site Shell!</h1>
 </div>
 
 <div class="clear"></div>
 
-<div class="grid_6">
-    This is my simple site shell!
+<div class="grid_8">
+    <h2>How do I use it?</h2>
     <p>
         Create your pages in  the web root:
         <pre>
-            $page_title = "Name Your Pages";
-            $meta_description = "Describe this page.";
-            $meta_keywords = "give, us, a few, keywords";
-            &lt;?php include("templates/head.php") ?&gt;
+            &lt;?php
+                $page_title = "Name Your Pages";
+                $meta_description = "Describe this page.";
+                $meta_keywords = "give us, a few, keywords";
+                include("templates/head.php")
+            ?&gt;
 
             [grid layout / page markup and content]
 
@@ -35,12 +37,21 @@ include("templates/head.php");
     </p>
 </div>
 
-<div class="grid_6">
-    <p>
-        It uses <a href="http://960.gs/" target="_blank">960.gs</a> for the layout and <a href="http://jquery.com/" target="_blank">jQuery</a> for your scripting needs.
-    </p>
+<div class="grid_8">
+    <h2>What's Included?</h2>
+    <ul>
+        <li>A basic HTML5 template</li>
+        <li><a href="http://960.gs/" target="_blank">960.gs</a> for the layout</li>
+        <li><a href="http://jquery.com/" target="_blank">jQuery</a> for your scripting needs</li>
+    </ul>
 </div>
 
 <div class="clear"></div>
+
+<div class="grid_12">
+    <p>
+        <a href="http://github.com/rogerd330/site-shell" target="_blank">Site-Shell on GitHub</a> by <a href="http://twitter.com/rogerdickeyjr" target="_blank">@rogerdickeyjr</a>
+    </p>
+</div>
 
 <?php include("templates/footer.php") ?>
